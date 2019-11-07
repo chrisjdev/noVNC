85d521a418ccf6df4e39082c3eaeb4a18cf158cf
Chris J <chrisjdev@users.noreply.github.com>
2019-11-07 15:15:15 -0500

    Fix for issue #1317
    Checking password for undefined instead of falsy. That way an empty password string is allowed.

--- core/rfb.js
+++ core/rfb.js
@@ -961,7 +961,7 @@ export default class RFB extends EventTargetMixin {
     _negotiate_std_vnc_auth() {
         if (this._sock.rQwait("auth challenge", 16)) { return false; }
 
-        if (!this._rfb_credentials.password) {
+        if (this._rfb_credentials.password === undefined) {
             this.dispatchEvent(new CustomEvent(
                 "credentialsrequired",
                 { detail: { types: ["password"] } }));
